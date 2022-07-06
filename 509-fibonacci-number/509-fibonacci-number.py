5ef548b826241c02e8aec64c8e03c74eb2f63b9d
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2022-07-06 21:08:26 +0530

    Time: 70 ms (30.71%), Space: 13.8 MB (95.78%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,15 +1,4 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if(n==0):
-            return 0
-        if(n==1):
-            return 1
-        a=0
-        b=1
-        
-        for _ in range(2,n+1):
-            c=a+b
-            a=b
-            b=c
-        
-        return c
+        golden_ratio=(1+5**0.5)/2
+        return int((golden_ratio**n+1)/5 **0.5)
