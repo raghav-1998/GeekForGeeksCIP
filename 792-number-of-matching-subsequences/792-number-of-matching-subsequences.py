cff383460b9e3a6289f47ed0449bf22e7d27fadb
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2022-07-20 23:25:00 +0530

    Time: 828 ms (47.17%), Space: 15.6 MB (51.10%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self, word):
+        self.word = word
+        self.index = 0
+        
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        buckets = defaultdict(list)
+        for word in words:
+            startingChar = word[0]
+            buckets[startingChar].append(Node(word))
+
+        ans = 0
+        for c in s:
+            currBucket = buckets[c]
+            buckets[c] = []
+            for node in currBucket:
+                node.index += 1  # Point to next character of node.word
+                if node.index == len(node.word):
+                    ans += 1
+                else:
+                    startingChar = node.word[node.index]
+                    buckets[startingChar].append(node)
+        return ans
