7e01701f9e14155ef4e4122fdc20fa7796a95a04
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2022-08-04 23:43:56 +0530

    Time: 41 ms (74.26%), Space: 13.9 MB (16.83%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        while(p%2==0 and q%2==0):
+            p//=2
+            q//=2
+        
+        if(q%2==0 and p%2!=0):
+            return 0
+        if(q%2!=0 and p%2==0):
+            return 2
+        if(q%2!=0 and p%2!=0):
+            return 1
+        
+        return -1
