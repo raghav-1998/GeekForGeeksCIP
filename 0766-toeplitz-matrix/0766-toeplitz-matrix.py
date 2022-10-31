4e08e296969a1dcb587509119f7c49225628edff
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2022-10-31 21:30:53 +0530

    Time: 145 ms (33.14%), Space: 13.4 MB (76.04%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        m=len(matrix)
+        n=len(matrix[0])
+        
+        for i in range(m-1):
+            for j in range(n-1):
+                if(matrix[i][j]!=matrix[i+1][j+1]):
+                    return False
+        
+        return True
