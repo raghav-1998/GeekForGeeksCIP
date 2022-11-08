5b82dc29a1dade175bb927c5830764279639e69c
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2022-11-08 23:59:07 +0530

    Time: 50 ms (45.24%), Space: 13.6 MB (12.86%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def makeGood(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = []
+        for c in s:
+            if not result:
+                result.append(c)
+            elif result[-1].isupper() and result[-1].lower() == c:
+                result.pop()
+            elif result[-1].islower() and result[-1].upper() == c:
+                result.pop()
+            else:
+                result.append(c)
+        return ''.join(result)
