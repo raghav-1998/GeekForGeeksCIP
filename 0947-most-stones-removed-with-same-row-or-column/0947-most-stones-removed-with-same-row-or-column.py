53f372fa1b49b78f1abd78c0133fb183a1ab6430
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2022-11-15 00:00:34 +0530

    Time: 325 ms (58.62%), Space: 15.8 MB (20.20%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def removeStones(self, stones):
+        """
+        :type stones: List[List[int]]
+        :rtype: int
+        """
+        def dfs(i, j):
+            points.discard((i, j))
+            for y in rows[i]:
+                if (i, y) in points:
+                    dfs(i, y)
+            for x in cols[j]:
+                if (x, j) in points:
+                    dfs(x, j)
+        points, island, rows, cols = {(i, j) for i, j in stones}, 0, collections.defaultdict(list), collections.defaultdict(list)
+        for i, j in stones:
+            rows[i].append(j)
+            cols[j].append(i)
+        for i, j in stones:
+            if (i, j) in points:
+                dfs(i, j)
+                island += 1
+        return len(stones) - island
+        
