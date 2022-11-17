d49df1ccfb22f7f71373b7a5b2e050e46e8bd7bb
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2022-11-17 23:49:11 +0530

    Time: 95 ms (31.46%), Space: 13.5 MB (15.73%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def computeArea(self, ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
+        """
+        :type ax1: int
+        :type ay1: int
+        :type ax2: int
+        :type ay2: int
+        :type bx1: int
+        :type by1: int
+        :type bx2: int
+        :type by2: int
+        :rtype: int
+        """
+        overlap = max(min(ax2,bx2)-max(ax1,bx1), 0)*max(min(ay2,by2)-max(ay1,by1), 0)
+        return (ax1-ax2)*(ay1-ay2) + (bx1-bx2)*(by1-by2) - overlap
