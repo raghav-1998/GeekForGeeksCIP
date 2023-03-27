8c390d1ab67cd388669638cc09eb9079f331c2f5
raghav-1998 <99480168+raghav-1998@users.noreply.github.com>
2023-03-27 23:53:18 +0530

    Added solution - LeetHub

---
+++ Divide in Incremental Groups - GFG/divide-in-incremental-groups.py
@@ -0,0 +1,55 @@
+#User function Template for python3
+
+class Solution:
+    def countWaystoDivide(self, N, K):
+        a=[]
+        for i in range(1,N+1):
+            a.append(i)
+        n=N
+        k=K
+        dp=[]
+        for i in range(N+1):
+            c=[]
+            for j in range(N+1):
+                d=[]
+                for k in range(k+1):
+                    d.append(-1)
+                c.append(d)
+            dp.append(c)
+        def coin(i,su,a1):
+            if(su==n):
+                if(a1==k):
+                    return 1
+                else:
+                    return 0
+            if(su>n or i>=n or a1>k):
+                return 0
+            if(dp[i][su][a1]!=-1):
+                return dp[i][su][a1]
+            a3=coin(i,su+a[i],a1+1)
+            a2=coin(i+1,su,a1)
+            dp[i][su][a1]=(a3+a2)
+            return dp[i][su][a1]
+        return(coin(0,0,0))
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(1000000)
+
+if __name__ == '__main__':
+    
+    T = int(input())
+    while T > 0: 
+        N =int(input())
+        K=int(input())
+        ob = Solution()
+        print(ob.countWaystoDivide(N,K))
+        
+        T -= 1
+# } Driver Code Ends
